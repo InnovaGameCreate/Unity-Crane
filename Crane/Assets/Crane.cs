4599bca7430bb86f17abecbaf1f6342d169e1b8e
石田翔太 <c611621050@tokushima-u.ac.jp>
2017-03-23 07:19:29 +0900

    First Commit

---
+++ Crane/Assets/Crane.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crane : MonoBehaviour {
+    public float MoveSpeed;
+    private float LimitDown = -10;
+    public GameObject crane;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKey(KeyCode.G))
+        {
+            transform.Rotate(new Vector3(0, 0, 1), -MoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            transform.Rotate(new Vector3(0, 0, 1), MoveSpeed);
+        }
+
+    }
+}
---
+++ Crane/Assets/testrotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testrotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, 100 * Time.fixedDeltaTime);
+	}
+}
