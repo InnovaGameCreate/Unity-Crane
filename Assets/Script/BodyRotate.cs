3dc807cea0784b8dec72fc5747119ff4f8cd5728
石田翔太 <c611621050@tokushima-u.ac.jp>
2017-03-24 23:37:16 +0900

    クレーン横回転実装+アーム（仮）追加
    a

---
+++ Assets/Script/BodyRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyRotate : MonoBehaviour {
+    public float RotateSpeed = 5;
+    private float Position;
+	// Use this for initialization
+	void Start () {
+        Position = gameObject.transform.rotation.y;
+           
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, Position, 0);
+        if (Input.GetKey(KeyCode.H))
+        {
+            transform.Rotate(0, Position, 0);
+            Position += RotateSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            transform.Rotate(0, Position, 0);
+            Position -= RotateSpeed;
+        }
+		
+	}
+}
