4ced9b855482d8d05d3176a442d5252147240405
user <user@USER-PC>
2017-04-02 15:16:25 +0900

    車体の安定化・カメラブレ修正

--- Assets/Script/SimpleCarController.cs
+++ Assets/Script/SimpleCarController.cs
@@ -12,6 +12,9 @@ public class SimpleCarController : MonoBehaviour
     public WheelCollider FrontLeft;
     public WheelCollider RearRight;//後輪
     public WheelCollider RearLeft;
+    float Breaking = 1000;
+
+    private Rigidbody rb;
 
     void Start()
     {
@@ -21,14 +24,21 @@ public class SimpleCarController : MonoBehaviour
         RearLeft.steerAngle = -90;
         RearRight.steerAngle = -90;
 
+        rb = GetComponent<Rigidbody>();
+        Application.targetFrameRate = 60;
     }
 
     void Update()
     {
+        //重心計算
+        rb.ResetCenterOfMass();
+        Vector3 mass = rb.centerOfMass;
+        mass.y -= 1;
+        rb.centerOfMass = mass;
 
         //移動
-        RearRight.motorTorque = Input.GetAxis("Vertical")*Speed;
-        RearLeft.motorTorque = Input.GetAxis("Vertical")*Speed;
+        RearRight.motorTorque = Input.GetAxis("Vertical") * Speed;
+        RearLeft.motorTorque = Input.GetAxis("Vertical") * Speed;
 
         //タイヤの角度変更(なぜかＷＡＳＤでも動いた)
         if (Input.GetKey(KeyCode.RightArrow) && FrontRight.steerAngle < -90 + Maxsteer)
@@ -44,10 +54,25 @@ public class SimpleCarController : MonoBehaviour
             FrontLeft.steerAngle -= RotateSpeed;
 
         }
+        if(Input.GetKeyUp(KeyCode.RightArrow)|| Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            FrontRight.steerAngle = FrontLeft.steerAngle =-90 ;
+           
+        }
+
+        RearLeft.brakeTorque = 0;
+        RearRight.brakeTorque =0;
+        // SPACEを押せばブレーキ。 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // ブレーキをかける値に代入。 
+            RearLeft.brakeTorque = Breaking;
+            RearRight.brakeTorque = Breaking;
+        }
 
 
-
-
+   
+      
 
 
 
