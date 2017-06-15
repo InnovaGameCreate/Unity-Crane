1e137670c80d2e91846c764427e0fdcff39c5f3a
石田翔太 <c611621050@tokushima-u.ac.jp>
2017-06-16 07:38:08 +0900

    スペースでのアーム開閉，爪へのあたり判定，運ぶものとの接触でアニメーション一時停止

---
+++ Assets/Script/ArmClawAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmClawAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private float defaultSpeed;
+    public Collision arm1;
+    public Collision arm2;
+    public Collision arm3;
+    public Collision arm4;
+    public GameObject a;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        defaultSpeed = anim.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Space))//スぺースでアームが開き始める
+        {
+            anim.SetBool("ArmStart", true);
+        }
+        else anim.SetBool("ArmStart", false);
+
+
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // 衝突した相手が運ぶものなら処理をする
+        if (collision.gameObject.name == "運ぶ物")
+        {
+            anim.speed = 0;
+
+            // 衝突した対象(collisionオブジェクト)の色を変更している。
+            //collision.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else
+            anim.speed = defaultSpeed;
+    }
+}
+
+        
