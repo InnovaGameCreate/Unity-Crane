22110a6f221c6ab13de3993ba0838b30a1969304
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2017-06-25 03:31:29 +0900

    スタート画面-スクリプトではなくアニメーターで遷移

--- Assets/Script/UI/Start/StartSceneManeger.cs
+++ Assets/Script/UI/Start/StartSceneManeger.cs
@@ -2,30 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartSceneManeger : MonoBehaviour
 {
-    [SerializeField]
-    private Canvas select_canvas;
-    [SerializeField]
-    private Canvas start_canvas;
-
-    private void Start()
-    {
-        select_canvas.enabled = false;
-    }
-
-    public void EnableSelectCanvas()
-    {
-        start_canvas.enabled = false;
-        select_canvas.enabled = true;
-    }
-
-    public void EnableStartCanvas()
-    {
-        start_canvas.enabled = true;
-        select_canvas.enabled = false;
-    }
 
     public void LoadScene1()
     {
