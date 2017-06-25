0afc92c3e11c9144f198f2138fc5fb8f06f3325a
umekawa-toshiki <mahouka.sao.kinsho315@gmail.com>
2017-06-25 21:36:54 +0900

    荒野ギミックあり
    バージョン低かったので、箱の耐久値はありません。

---
+++ Assets/Script/Duststorm.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duststorm : MonoBehaviour {
+    private float time = 0;
+    private ParticleSystem storm;
+    private AudioSource sound1;
+    private AudioSource sound2;
+    //private TargetStatus damage;
+
+    void Start () {
+        storm = GetComponent<ParticleSystem>();
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        sound1 = audioSources[0];
+        sound1 = audioSources[1];
+
+        //damage = GetComponent<TargetStatus>();
+
+        //初期値のランダム設定
+        transform.position = new Vector3(Random.Range(30.0f, 420.0f), 2, Random.Range(30.0f, 420.0f));
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        //enabled = !enabled;
+        if(time >= 5.0 && time < 10.0)
+        {
+            sound1.PlayOneShot(sound1.clip);
+        }
+        else if (time >= 10.0 && time < 20.0)
+        {
+            storm.Play();
+            sound2.PlayOneShot(sound2.clip);
+        }
+        else if (time >= 20.0)
+        {
+            storm.Stop();
+            storm.Clear();
+            time = 0;
+            transform.position = new Vector3(Random.Range(30.0f, 420.0f), 2, Random.Range(30.0f, 420.0f));
+        }
+    }
+
+    //竜巻に入っているときにダメージを受ける
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("MovableObj"))
+        {
+            //damage.
+        }
+    }
+}
