73fd4eeeea5b156e69661d534feeef3e6f2f2254
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2017-06-27 12:43:59 +0900

    ビルド時に遊び方が表示されなくなったので修正

---
+++ Assets/Script/UI/Start/UpdateRectTransform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateRectTransform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //ScrollRectが更新されず子要素が表示されないので値を変えて無理やり更新
+        GetComponent<RectTransform>().anchorMax = new Vector2(1, 1);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
